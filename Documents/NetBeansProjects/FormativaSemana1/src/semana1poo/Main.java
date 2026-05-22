81a489fbea1bd84ea509b65dd36dca46c2f03e85
cncchild <cncchild@gmail.com>
2026-05-22 12:12:18 -0400

    Cuarto commit viernes de la formativa 1 semana  1

--- Documents/NetBeansProjects/FormativaSemana1/src/semana1poo/Main.java
+++ Documents/NetBeansProjects/FormativaSemana1/src/semana1poo/Main.java
@@ -8,54 +8,28 @@ public class Main {
 
     public static void main(String[] args) {
         System.setOut(new java.io.PrintStream(System.out, true, java.nio.charset.StandardCharsets.UTF_8));
-        // =========================
         // OBJETO 1
-        // =========================
-
-        EstudianteInstitucional estudiante1 =
-                new EstudianteInstitucional(
+        EstudianteInstitucional estudiante1 =  new EstudianteInstitucional(
                         "Cristian",
                         30,
                         "Diseño y Desarrollo Web",
                         true,
                         6.5
                 );
-
-        // =========================
         // OBJETO 2
-        // =========================
 
-        EstudianteInstitucional estudiante2 =
-                new EstudianteInstitucional(
+        EstudianteInstitucional estudiante2 = new EstudianteInstitucional(
                         "Camila",
                         22,
                         "Enfermería",
                         true,
                         5.8
                 );
-                      // =========================
-        // OBJETO 3
-        // =========================
-               EstudianteInstitucional estudiante3 =
-                new EstudianteInstitucional(
-                        "Catalina",
-                        15,
-                        "Media",
-                        true,
-                        6.8
-                );
-
-        // =========================
         // MOSTRAR DATOS
-        // =========================
-
         System.out.println(estudiante1);
-
         System.out.println("-------------------");
-
         System.out.println(estudiante2);
-            System.out.println("-------------------");
-               System.out.println(estudiante3);
+        System.out.println("-------------------");
 
     }
 
