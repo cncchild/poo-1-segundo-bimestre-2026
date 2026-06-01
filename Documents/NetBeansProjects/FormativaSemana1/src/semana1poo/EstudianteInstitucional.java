34dc6d6a5f17351fa9723eae18986c9f77094496
cncchild <cncchild@gmail.com>
2026-06-01 19:39:49 -0400

    sexto commit viernes de la formativa 1 semana  1

--- Documents/NetBeansProjects/FormativaSemana1/src/semana1poo/EstudianteInstitucional.java
+++ Documents/NetBeansProjects/FormativaSemana1/src/semana1poo/EstudianteInstitucional.java
@@ -44,7 +44,6 @@ public class EstudianteInstitucional {
             boolean estadoMatricula,
             double promedio) {
 
-        this.nombre = nombre;
         setNombre(nombre);
         setEdad(edad);
         setCarrera(carrera);
